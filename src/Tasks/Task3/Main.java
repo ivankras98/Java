b802682040a1300774a4003c93b22faff2fc2a90
Krasilnikov_Ivan <ikra-nn@yandex.ru>
2024-03-10 11:10:03 +0300

    Исправил Task3

--- src/Tasks/Task3/Main.java
+++ src/Tasks/Task3/Main.java
@@ -1,31 +1,41 @@
-/*Задача № 4
-Определить одномерный массив и заполнить его случайными значениями.
-Определить дополнительный массив разрешенных значений. Определить и вывести на экран, сколько элементов исходного массива имеют разрешенные значения
-*/
-
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        // Число элементов в массиве
         int n = 10;
+
+        // Создаем одномерный массив и заполняем его случайными значениями
         int[] array = new int[n];
-        Random random = new Random ();
-        for (int i = 0; i<n; i++){
-            array[i] = random.nextInt(10);
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            array[i] = random.nextInt(10); // Генерируем случайное число от 0 до 9
         }
-        System.out.println ("Исходный массив: ");
+
+        // Выводим основной массив
+        System.out.println("Основной одномерный массив:");
         System.out.println(Arrays.toString(array));
+
+        // Определяем дополнительный массив разрешенных значений
         int[] allowedValues = {2, 4, 6, 8};
+
+        // Выводим дополнительный массив разрешенных значений
+        System.out.println("Дополнительный массив разрешенных значений:");
+        System.out.println(Arrays.toString(allowedValues));
+
+        // Подсчитываем количество элементов исходного массива, которые имеют разрешенные значения
         int count = 0;
         for (int num : array) {
             for (int allowed : allowedValues) {
                 if (num == allowed) {
-                    count ++;
-                    break;
+                    count++;
+                    break; // Выходим из цикла, если значение найдено
                 }
             }
         }
+
+        // Выводим результат на экран
         System.out.println("Количество элементов исходного массива с разрешенными значениями: " + count);
     }
-    
 }
